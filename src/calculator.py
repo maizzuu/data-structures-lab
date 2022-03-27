2c6217d2c22f021eb16c383ae285b93e5d67e1c5
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-03-27 21:54:09 +0300

    Separated algorith to its own class

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,5 @@
 from calculatorIO import CalculatorIO
-
-operators = {
-    "+": {"precedence": 2, "associativity": "Left"},
-    "-": {"precedence": 2, "associativity": "Left"},
-    "*": {"precedence": 3, "associativity": "Left"},
-    "/": {"precedence": 3, "associativity": "Left"},
-    "^": {"precedence": 4, "associativity": "Right"}
-}
+from shunting_yard import ShuntingYard
 
 
 class Calculator:
@@ -18,74 +11,24 @@ class Calculator:
     """
 
     def __init__(self, io=CalculatorIO()):
-        self.expression = ""
-        self.output = []
-        self.operators = []
         self.io = io
 
     def algorithm(self, expression):
-        # type isn't needed until functions are included
-        previous = {"input": "", "type": ""}
-        for i, n in enumerate(expression):
-            token = n
-            next_token = expression[i+1] if i < (len(expression)-1) else None
-
-            if token in "0123456789":
-                if not next_token:
-                    self.output.append(previous["input"] + token)
-                elif next_token in "0123456789":
-                    previous["input"] += token
-                else:
-                    self.output.append(previous["input"] + token)
-                    previous["input"] = ""
-
-            elif token in operators:
-                if not self.operators:
-                    self.operators.append(token)
-                else:
-                    while (
-                        self.operators[-1] != "("
-                        and ((operators[self.operators[-1]]["precedence"]
-                              > operators[token]["precedence"])
-                             or (operators[self.operators[-1]]["precedence"]
-                             == operators[token]["precedence"]
-                                 and operators[token]["associativity"] == "Left"))
-                    ):
-                        self.output.append(self.operators.pop())
-                        if len(self.operators) == 0:
-                            break
-                    self.operators.append(token)
-            elif token == "(":
-                self.operators.append(token)
-            elif token == ")":
-                while True:
-                    try:
-                        top = self.operators.pop()
-                    except IndexError:
-                        self.io.write("ERROR: mismatched parentheses")
-                        break
-                    if top != "(":
-                        self.output.append(top)
-                        continue
-                    break
-        while self.operators:
-            self.output.append(self.operators.pop())
+        pass
 
     def start(self):
         while True:
-            self.expression = self.io.read()
+            expression = self.io.read()
             print()
-            if self.expression == "":
+            if expression == "":
                 self.io.write("Quitting calculator")
                 break
-            if self.expression == "help":
-                self.io.write("instructions")
-                print()
+            if expression == "help":
+                self.instructions()
             else:
-                self.algorithm(self.expression)
-                self.io.write(" ".join(self.output))
-                self.clear()
+                algorithm = ShuntingYard(expression)
+                rpn = algorithm.parse()
+                self.io.write(rpn)
 
-    def clear(self):
-        self.output = []
-        self.operators = []
+    def instructions(self):
+        self.io.write("instructions")
---
+++ src/shunting_yard.py
@@ -0,0 +1,79 @@
+operators = {
+    "+": {"precedence": 2, "associativity": "Left"},
+    "-": {"precedence": 2, "associativity": "Left"},
+    "*": {"precedence": 3, "associativity": "Left"},
+    "/": {"precedence": 3, "associativity": "Left"},
+    "^": {"precedence": 4, "associativity": "Right"}
+}
+
+
+class ShuntingYard:
+    def __init__(self, expression: str):
+        self.expression = expression
+        self.output = []
+        self.opstack = []
+        # type isn't needed until functions are included
+        self.previous = {"input": "", "type": ""}
+
+    def parse(self):
+
+        for index, token in enumerate(self.expression):
+            try:
+                next_token = self.expression[index+1]
+            except IndexError:
+                next_token = None
+
+            if token in "0123456789":
+                self.number(token, next_token)
+
+            elif token in operators:
+                self.operator(token)
+
+            elif token in ("(", ")"):
+                try:
+                    self.parentheses(token)
+                except IndexError:
+                    return "ERROR: mismatched parentheses"
+            else:
+                return "ERROR: unknown input"
+
+        while self.opstack:
+            self.output.append(self.opstack.pop())
+        return " ".join(self.output)
+
+    def number(self, token: str, next_token: str | None):
+        if not next_token:
+            self.output.append(self.previous["input"] + token)
+        elif next_token in "0123456789":
+            self.previous["input"] += token
+        else:
+            self.output.append(self.previous["input"] + token)
+            self.previous["input"] = ""
+
+    def operator(self, token: str):
+        if not self.opstack:
+            self.opstack.append(token)
+        else:
+            while (
+                self.opstack[-1] != "("
+                and ((operators[self.opstack[-1]]["precedence"]
+                      > operators[token]["precedence"])
+                     or (operators[self.opstack[-1]]["precedence"]
+                         == operators[token]["precedence"]
+                         and operators[token]["associativity"] == "Left"))
+            ):
+                self.output.append(self.opstack.pop())
+                if len(self.opstack) == 0:
+                    break
+            self.opstack.append(token)
+
+    def parentheses(self, token: str):
+        if token == "(":
+            self.opstack.append(token)
+        elif token == ")":
+            while True:
+                top = self.opstack.pop()
+                if top != "(":
+                    self.output.append(top)
+                    continue
+                break
