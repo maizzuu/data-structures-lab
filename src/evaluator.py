2b825e2e95e06439c4b078ef604770a73e21d782
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-03-30 15:37:38 +0300

    Documentation

--- src/evaluator.py
+++ src/evaluator.py
@@ -2,16 +2,36 @@ from shunting_yard import operators
 
 
 class Evaluator:
+    """This class calculates the result of an equation in reverse Polish notation.
+
+    Attributes:
+        expression: The equation as a string.
+        operands: An empty list for storing the operands.
+    """
+
     def __init__(self, expression: str):
+        """The constrcutor for this class.
+
+        Args:
+            expression (str): The equation as a string.
+        """
         self.expression = expression.split(" ")
         self.operands = []
 
     def evaluate(self) -> int:
+        """This method iterates the equation and is in charge of handling the tokens.
+
+        Returns:
+            int: Returns the final result of the equation.
+        """
         if len(self.expression) == 1:
             return int(self.expression[0])
         for token in self.expression:
             if token not in operators:
-                self.operands.append(int(token))
+                try:
+                    self.operands.append(int(token))
+                except ValueError:
+                    self.operands.append(float(token))
             else:
                 second = self.operands.pop()
                 first = self.operands.pop()
@@ -20,6 +40,16 @@ class Evaluator:
         return self.operands.pop()
 
     def calculate(self, operator: str, first: int, second: int) -> int:
+        """This method returns the result of one operation.
+
+        Args:
+            operator (str): The operation in question.
+            first (int): The first operand.
+            second (int): The second operand.
+
+        Returns:
+            int: The result of the operation.
+        """
         if operator == "+":
             return first + second
         if operator == "-":
@@ -27,6 +57,5 @@ class Evaluator:
         if operator == "*":
             return first * second
         if operator == "/":
-            return first / second
-        if operator == "^":
-            return first ** second
+            return round(first / second, 3)
+        return first ** second  # if operator == "^"
