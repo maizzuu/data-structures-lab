c79f8d584f6fb80f508c86038a6cd562f06ea4b8
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-03-30 15:38:21 +0300

    Redid most test

--- src/tests/calculator_test.py
+++ src/tests/calculator_test.py
@@ -15,75 +15,124 @@ class StubIO:
 
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.io = StubIO([])
+        self.calc = Calculator(self.io)
+
     def test_empty_expression_quits(self):
-        io = StubIO([""])
+        self.io.inputs = [""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "Quitting calculator")
+        self.assertEqual(self.io.outputs[0], "Quitting calculator")
 
     def test_input_help_for_instructions(self):
         # the last item in the input list must always be "" to stop the running
-        io = StubIO(["help", ""])
+        self.io.inputs = ["help", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "instructions")
+        self.assertEqual(self.io.outputs[0], "instructions")
 
-    def test_algorithm_basic(self):
-        io = StubIO(["12+3", ""])
+    def test_correct_rpn_result_plus(self):
+        self.io.inputs = ["5+2", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "12 3 +")
+        self.assertEqual(self.calc.rpn, "5 2 +")
+        self.assertEqual(self.io.outputs[0], 7)
 
-    def test_algorithm_two_operations(self):
-        io = StubIO(["1+2-3", ""])
+    def test_correct_rpn_result_minus(self):
+        self.io.inputs = ["7-2", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "1 2 + 3 -")
+        self.assertEqual(self.calc.rpn, "7 2 -")
+        self.assertEqual(self.io.outputs[0], 5)
 
-    def test_algorithm_with_parentheses(self):
-        io = StubIO(["2*(3-2)", ""])
+    def test_correct_rpn_result_multiplication(self):
+        self.io.inputs = ["3*2", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "2 3 2 - *")
+        self.assertEqual(self.calc.rpn, "3 2 *")
+        self.assertEqual(self.io.outputs[0], 6)
 
-    def test_mismatched_parentheses_cause_an_error(self):
-        io = StubIO(["2*()))", ""])
+    def test_correct_rpn_result_division(self):
+        self.io.inputs = ["6/2", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "ERROR: mismatched parentheses")
+        self.assertEqual(self.calc.rpn, "6 2 /")
+        self.assertEqual(self.io.outputs[0], 3)
 
-    def test_algorithm_iterates_stack(self):
-        io = StubIO(["3*4*(2+5)", ""])
+    def test_division_rounded_result_correct(self):
+        self.io.inputs = ["100/3", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "3 4 * 2 5 + *")
+        self.assertEqual(self.io.outputs[0], 33.333)
 
-    def test_algorithm_long_expression(self):
-        io = StubIO(["1*(4+5)^8+5+(8-4)*5", ""])
+    def test_correct_rpn_result_power(self):
+        self.io.inputs = ["4^3", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "1 4 5 + 8 ^ * 5 + 8 4 - 5 * +")
+        self.assertEqual(self.calc.rpn, "4 3 ^")
+        self.assertEqual(self.io.outputs[0], 64)
 
-    def test_unknown_input(self):
-        io = StubIO(["1+2€", ""])
+    def test_input_len_1(self):
+        self.io.inputs = ["2", ""]
 
-        calc = Calculator(io)
-        calc.start()
+        self.calc.start()
 
-        self.assertEqual(io.outputs[0], "ERROR: unknown input")
+        self.assertEqual(self.calc.rpn, "2")
+        self.assertEqual(self.io.outputs[0], 2)
+
+    def test_mismatched_parentheses_error(self):
+        self.io.inputs = ["2*4-5)", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "ERROR: mismatched parentheses")
+        self.assertEqual(self.io.outputs[0], "ERROR: mismatched parentheses")
+
+    def test_input_with_decimal(self):
+        self.io.inputs = ["3.5*2", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "3.5 2 *")
+        self.assertEqual(self.io.outputs[0], 7)
+
+    def test_expression_ends_in_period_error(self):
+        self.io.inputs = ["4*6+5.", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "ERROR: invalid input")
+        self.assertEqual(self.io.outputs[0], "ERROR: invalid input")
+
+    def test_invalid_input(self):
+        self.io.inputs = ["4€+3", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "ERROR: invalid input")
+        self.assertEqual(self.io.outputs[0], "ERROR: invalid input")
+
+    def test_parentheses(self):
+        self.io.inputs = ["3*(4+6)^3-(2+1)*4", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "3 4 6 + 3 ^ * 2 1 + 4 * -")
+        self.assertEqual(self.io.outputs[0], 2988)
+
+    def test_mismatched_parenthesis_at_end(self):
+        self.io.inputs = ["3*(4+8)(", ""]
+
+        self.calc.start()
+
+        self.assertEqual(self.calc.rpn, "ERROR: mismatched parentheses")
+        self.assertEqual(self.io.outputs[0], "ERROR: mismatched parentheses")
