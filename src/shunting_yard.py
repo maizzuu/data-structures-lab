43cbb9974bf22d5b8592da16369a8428e0883a7d
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-04-05 12:03:22 +0300

    Documentation

--- src/shunting_yard.py
+++ src/shunting_yard.py
@@ -41,8 +41,8 @@ class ShuntingYard:
     def parse(self) -> str:
         """Method that is in charge of parsing the expression and returning the final output.
 
-        When the current token is a parenthesis, the method excepts
-        an IndexError in case the parentheses are mismatched.
+        This method will go through each token in the expression, check its type and
+        handle it accordingly, also check the expression for any invalidities.
 
         Returns:
             str: The given expression in reverse polish notation.
@@ -85,19 +85,23 @@ class ShuntingYard:
             else:
                 return "ERROR: invalid input"
 
-        while self.opstack:
-            if "(" in self.opstack or ")" in self.opstack:
-                return "ERROR: mismatched parentheses"
-            self.output.append(self.opstack.pop())
+        try:
+            self.finish()
+        except InputError:
+            return "ERROR: mismatched parentheses"
+
         return " ".join(self.output)
 
-    # can't define the type of next_token because it fails the CI build
-    def number(self, token: str, next_token: str | None):
-        """_summary_
+    # can't define the type of next_token here because it fails the CI build
+    def number(self, token: str, next_token):
+        """A method for handling a number token.
+
+        The number will be stored if the next token is also a number,
+        otherwise it will be added to the output.
 
         Args:
-            token (str): _description_
-            next_token: _description_
+            token (str): Current token, a number.
+            next_token(str | None): The next token.
         """
         if not next_token:
             self.output.append(self.previous["input"] + token)
@@ -108,6 +112,19 @@ class ShuntingYard:
             self.previous["input"] = ""
 
     def period(self, token: str, next_token, index: int):
+        """A method for handling a period token.
+
+        Will either raise an error or store the token in the case the next token is a number.
+
+        Args:
+            token (str): Current token, a period.
+            next_token (str | None): The next token.
+            index (int): The index of the current token.
+
+        Raises:
+            InputError: The first InputError will be raised if the expression starts with a period.
+            InputError: The second InputError will be raised if next_token isn't a number.
+        """
         if index == 0:
             raise InputError
         if not next_token or next_token not in "0123456789":
@@ -139,26 +156,68 @@ class ShuntingYard:
     def parentheses(self, token: str):
         """A method for handling a parenthesis token.
 
+        All left parentheses will be added to the operator stack, while a right parenthesis will
+        cause the iteration of the operator stack. If the top operator is a left parentheses, it
+        will just be popped from the stack. While the top operator is something else, it is added
+        to the output. If the stack is empty, an IndexError will be raised.
+
         Args:
             token (str): The left or right parenthesis.
+
+        Raises:
+            IndexError: Error raised when running out of operators looking for a left parenthesis.
         """
         if token == "(":
             self.opstack.append(token)
         else:  # token == ")"
             while True:
+                if not self.opstack:
+                    raise IndexError
                 top = self.opstack.pop()
                 if top != "(":
                     self.output.append(top)
                     continue
                 break
 
-    def check_adjacent_operators(self, token, next_token):
+    def check_adjacent_operators(self, token: str, next_token):
+        """A method for checking whether an operator is followed by another one.
+
+        Args:
+            token (str): The current token.
+            next_token (str | None): The next token.
+
+        Raises:
+            InputError: Error raised when token and next_token are both operators.
+        """
         if token in operators and next_token in operators:  # adjacent operators cause an error
             raise InputError
 
-    def minus(self, token, previous_token):
+    def minus(self, token: str, previous_token):
+        """A method for handling a minus token.
+
+        This method will check the type of the previous token to determine whether the
+        minus sign is an operator or a negation.
+
+        Args:
+            token (str): The current token, a minus sign.
+            previous_token (str | None): The previous token.
+        """
         # if the previous token is a number this is an operation otherwise a negation
         if previous_token is None or previous_token not in "0123456789":
             self.previous["input"] += token
         else:
             self.operator(token)
+
+    def finish(self):
+        """A method for the final step of the Shunting-yard algorithm.
+
+        This method will iterate through the remaining operator stack, checking whether
+        any parentheses remain and adding operators to the output.
+
+        Raises:
+            InputError: Raised when there is a parenthesis left in the stack.
+        """
+        while self.opstack:
+            if "(" in self.opstack or ")" in self.opstack:
+                raise InputError
+            self.output.append(self.opstack.pop())
