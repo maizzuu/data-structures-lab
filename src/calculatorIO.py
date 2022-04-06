cb298a6a3ddf39f9016f393c514c5853d3257253
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-04-06 13:57:58 +0300

    Updated calculatorIO.py

--- src/calculatorIO.py
+++ src/calculatorIO.py
@@ -1,7 +1,5 @@
 class CalculatorIO:
     def read(self):
-        print(
-            "Input an expression to calculate, leave empty to exit, help for instructions")
         input_str = input("Input: ")
         return input_str
 
