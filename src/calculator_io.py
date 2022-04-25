011f92c46d5f6c693684497aa70d05765dfdd911
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-04-25 14:26:59 +0300

    Added instructions

--- src/calculator_io.py
+++ src/calculator_io.py
@@ -1,3 +1,11 @@
+instructions = [
+    "Use periods to indicate decimal places",
+    "Trigonometric functions use radians",
+    "Variable names can only include lowercase letters",
+    "Variable value can only be a number",
+    "Functions must always be followed by a left parenthesis, i.e. 'ln 2' is not ok"]
+
+
 class CalculatorIO:
     def read(self, text="Input"):
         input_str = input(f"{text}: ")
@@ -5,6 +13,11 @@ class CalculatorIO:
 
     def write(self, output):
         print(output)
+        print()
+
+    def print_instructions(self):
+        for item in instructions:
+            print(item)
         print()
 
 
